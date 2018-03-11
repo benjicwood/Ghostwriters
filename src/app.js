42f2ac0480754e0fb1de1758081c3ecd08641322
benjicwood <bencharlwood@gmail.com>
2018-03-11 21:34:26 +0000

    add background image and bootstrap through html

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// import background from '../dist/images/htglogo.jpg'
+
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-      </div>
+        <img src='http://i1244.photobucket.com/albums/gg569/benjicwood/htglogo-1_zps1pebtllh.png' />        
+        <h1>Ghostwriters</h1>     
+         </div>
     )
   }
 }
