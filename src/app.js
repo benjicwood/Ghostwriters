3017752d3401797ac5eb6a78a6aaea7b2bfea5d0
benjicwood <bencharlwood@gmail.com>
2018-03-11 23:34:26 +0000

    add navbar

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import background from '../dist/images/htglogo.jpg'
+import NavBar from './Nav/NavBar'
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <img src='http://i1244.photobucket.com/albums/gg569/benjicwood/htglogo-1_zps1pebtllh.png' />        
-        <h1>Ghostwriters</h1>     
+        <img src='http://i1244.photobucket.com/albums/gg569/benjicwood/htglogo-1_zps1pebtllh.png' className="img-fluid" alt="Responsive image" style={backgroundimage} />        
+        <NavBar />    
          </div>
     )
   }
 }
+
+const backgroundimage = {
+    margin: 'auto',
+    align: 'center'
+  };
 
 const app = document.getElementById('app')
 ReactDOM.render(<Main />, app)
